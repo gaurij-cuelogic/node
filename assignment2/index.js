18e7014e97a5878c9f3a0be1f28d5c53b7b0d821
“gaurijoshi” <gauri.joshi@cuelogic.com>
2018-12-04 15:55:46 +0530

    assignment2

--- assignment2/index.js
+++ assignment2/index.js
@@ -1,119 +1,93 @@
-var mongoose = require('mongoose');
 var express = require('express');
 var bodyParser = require('body-parser');
-
-
+var path = require('path');
+var mongoose = require ('mongoose');
 mongoose.connect('mongodb://localhost:27017/Company',{ useNewUrlParser: true },{autoIndex : false});
 
 var Schema = mongoose.Schema;
-var app = express()
+
+var userDataSchema = new Schema({
+    EID:{type:Number, unique:true},
+    First_Name:{type:String, required:true},
+    Last_Name:{type:String, required:true},
+    Salary:{type:Number, required:true}
+})
+
+var userData = mongoose.model('userData',userDataSchema);
+
+var app = express();
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('./public'))
+app.use(bodyParser.urlencoded({extended:false}));
 
+app.use(express.static('public'));
 
-var EmployeeSchema = new Schema({
-    EmpId: {
-        type: Number,
-        unique: true,
-    },
-    FullName: {
-        type: String,
-    },
-    Address: {
-        state: String,
-        city: String,
-        zip: Number
-    },
-    DateOfBirth: Date,
-    JoinDate: Date,
-    Salary: Number
-});
+app.post('/',function(req,res){
+    var item={
+        EID:req.body.eid,
+        First_Name:req.body.fname,
+        Last_Name:req.body.lname,
+        Salary:req.body.salary
+    }
 
-var EmployeeModel = mongoose.model('Employee', EmployeeSchema);
-
-
-var createData = (req, res, next) => {
-    //res.sendFile(path.join(__dirname, '/public/index.html'));
-    var employee = new EmployeeModel({
-        EmpId: req.body.Empid,
-        FullName: req.body.name,
-        Address: req.body.address,
-        DateOfBirth: req.body.BirthDate,
-        JoinDate: req.body.JoiningDate,
-        Salary: req.body.salary,
-    });
-
-    var data = new EmployeeModel(employee);
+    var data = new userData(item);
     data.save();
-    res.send(data);
-}
+    res.redirect('/')
+})
 
-var showDeletePage = (req,res,next) => {
+app.get('/css',function(req,res){
+    res.sendFile(path.join(__dirname+'/public'+'/main.css'))
+})
 
-    res.sendFile(path.join(__dirname+'/public'+'/delete.html'));
-  
-}
-
-var deleteEmployee = (req,res,next) => {
-
-    empData.findOneAndRemove({empId:req.body.empid}, function(err, employee) {
-        res.send("Deleted" + employee);
-    });
-   
-}
-
-var showUpdatePage = (req,res,next) => {
-    res.sendFile(path.join(__dirname+'/public'+'/update.html'));
-   
-
-
-}
-showEmployeeDetails = (req,res,next) => {
-    
-    empData.findOne({"empId":req.body.empid},function(err,employee){
-       
-        if(err) {
-            throw err;
+app.get('/update',function(req,res){
+    res.sendFile(path.join(__dirname+'/public'+'/update.html'),function(err){
+        if(err){
+            console.log(err);
         }
-    });
-    
-}
-
-
-
-var updateEmployee = (req,res,next) => {
-    var query = {empId: req.body.empid};  
-    var updateData = {
-        empId: req.body.empid,
-        firstName: req.body.fname,
-        lastName: req.body.lname,
-        age: req.body.age,
-    }  
-    
-    empData.findOneAndUpdate(query,updateData, function(err,data) {
-        if(err) { throw err }
-
-        res.send("Updated Data" + data);
-        
     })
-}
+})
 
-var showUpdatePages = (req,res,next) => {
-    res.redirect('/updateEmployee');
-}
+app.post('/update',function(req,res){
+    userData.findOne({'EID':req.body.eid},function(err,users){
+        if(err){
+            console.log(err);
+        }else{
+            if(users == null){
+                res.send("Enter valid user id");
+            }else{
+                users.EID = req.body.eid;
+                users.First_Name=req.body.fname,
+                users.Last_Name=req.body.lname,
+                users.Salary=req.body.salary
+                users.save();
+                res.send("done");
+            }
+            
+        }
+    })
+})
 
-app.get('/',createData);
-app.post('/',createData);
-app.get('/delete',showDeletePage);
-app.post('/delete',deleteEmployee);
-app.get('/update',showUpdatePage);
-// app.post('/updateEmployeeOne',showEmployeeDetails);
-// app.post('/updateEmployeeTwo',updateEmployee);
-// app.get('/updateEmployeeTwo',showUpdatePages);
+app.get('/delete',function(req,res){
+    res.sendFile(path.join(__dirname+'/public'+'/delete.html'),function(err){
+        if(err){
+            console.log(err);
+        }
+    })
+})
+
+app.post('/delete',function(req,res){
+   // console.log(req.body.eid);
+    userData.findOne({'EID':req.body.eid},function(err,users){
+        if(err){
+            console.log(err)
+        }else{
+            users.remove();
+            res.send("Succesfully Deleted"+req.body.eid);
+        }   
+    });   
+})
 
 app.listen(3000);
 
-
-
+console.log("server running at port 3000");
 
